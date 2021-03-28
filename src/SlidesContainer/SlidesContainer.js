8e4709b61d109671066b14aab01de8805d439da5
Donald Ballowe <dballowe7912@gmail.com>
2021-03-27 23:29:12 -0500

    Refactored Slide Show and mobile responsiveness for home page

--- src/SlidesContainer/SlidesContainer.js
+++ src/SlidesContainer/SlidesContainer.js
@@ -1,57 +1,74 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
+import imageOne from '../assets/photos/image-1.jpg';
+import imageTwo from '../assets/photos/image-2.jpg';
+import imageThree from '../assets/photos/image-3.jpg';
+import imageFour from '../assets/photos/image-4.jpg';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
-
 import './SlidesContainer.css'
 
-const SlidesContainer = ({images=[], interval=3000}) => {
+class SlidesContainer extends Component {
+  constructor(props) {
+    super(props);
 
-    const [thumbnails, setThumbnails] = useState([]);
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const [currentSlideStyle, setCurrentSlideStyle] = useState({});
-
-    useEffect(()=>{
-        setThumbnails(images);
-        setCurrentSlideStyle({
-            backgroundImage: "url('"+images[currentSlide]+"')",
-        });
-    }, [images, currentSlide]);
-
-    function previous(){
-        if(currentSlide>0){
-            setCurrentSlide(currentSlide-1);
-        }else{
-            setCurrentSlide(thumbnails.length-1);
-        }
+    this.state = {
+      currentImage: imageTwo,
+      currentImageIndex: 2,
+      imageArr: [imageOne, imageTwo, imageThree, imageFour]
     }
 
-    function next(){
-        if(currentSlide === thumbnails.length-1){
-            setCurrentSlide(0);
-        }else{
-            setCurrentSlide(currentSlide+1);
-        }
+    this.previous = this.previous.bind(this);
+    this.next = this.next.bind(this);
+  }
+
+  previous(){
+    let index = this.state.currentImageIndex;
+    if (index > 0) {
+      index --;
+    } else {
+      index = this.state.imageArr.length - 1;
     }
-    
-    
+    this.setState((st)=>({
+      currentImageIndex: index,
+      currentImage: this.state.imageArr[index]
+    }))
+  }
+
+  next() {
+    let index = this.state.currentImageIndex;
+    if (index < this.state.imageArr.length - 1) {
+      index ++;
+    } else {
+      index = 0;
+    }
+    this.setState((st)=>({
+      currentImageIndex: index,
+      currentImage: this.state.imageArr[index]
+    }))
+  }
+
+  render(){
+    let { currentImage, currentImageIndex } = this.state;
     return (
-    <div className='photo-slide'>
-        <section className="slideshow">
-            <div style={currentSlideStyle} className="slide-thumbnail">
-                <div className="slideshow-controller">
-                    <span className='left-button' onClick={previous}>
-                        <ArrowBackIosIcon id='arrow-left-button'/>
-                    </span>
-                    <span className='right-button' onClick={next}>
-                        <ArrowForwardIosIcon id='arrow-right-button'/>
-                    </span>
-                </div>
+        <div className="photo-slide">
+            <div className='slideshow'>
+              <img className='current-slide' src={currentImage} alt={currentImageIndex}/>
             </div>
-        </section>
-    </div>
-    )
+            <div className='slideshow-controller'>
+              <div className='slideshow-buttons'>
+                <span className='left-button' onClick={this.previous}>
+                  <ArrowBackIosIcon />
+                </span>
+                <span className='right-button' onClick={this.next}>
+                  <ArrowForwardIosIcon />
+                </span>
+              <div className='shop-btn'>Order Now</div>
+              </div>
+            </div>
+        </div>
+    );
+  }
 }
 
 export default SlidesContainer;
-
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,10 +3,6 @@ import SlidesContainer from '../../SlidesContainer/SlidesContainer';
 import Header from '../../Header/Header';
 import Navbar from '../../Navbar/Navbar';
 
-import image4 from '../../assets/photos/image-2.jpg';
-import image5 from '../../assets/photos/image-3.jpg';
-import image6 from '../../assets/photos/image-4.jpg';
-
 import './HomePage.css';
 import Footer from '../../Footer/Footer';
 
@@ -16,17 +12,10 @@ class HomePage extends Component {
             <div className='home-page'>
                 <Header />
                 <Navbar />
-                <div className='homePage-slideContainer'>
-                    <SlidesContainer 
-                        interval={3000}
-                        images={[
-                            image4,
-                            image5,
-                            image6,
-                        ]}
-                    />
+                <div className='slideshow-container'>
+                    <SlidesContainer />
                 </div>
-                <Footer/>
+                {/* <Footer/> */}
             </div>
         )
     }
